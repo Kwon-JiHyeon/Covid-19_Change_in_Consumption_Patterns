35a59be02746eb86461a1c8b84020ff99a8b9c5c
JIHYEON KWON <rnjswlgus98@gmail.com>
2023-08-22 11:02:18 +0900

    소상공인&전통시장 streamlit page

---
+++ page/BSI_proc_page.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 16 09:22:43 2023
+
+@author: rnjsd
+"""
+
+import streamlit as st
+from utils import BSI_processing as bp
+
+def app():
+    bp.proc()
+
+
