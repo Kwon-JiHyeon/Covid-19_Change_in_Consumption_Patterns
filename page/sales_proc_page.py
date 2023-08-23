d392edebc376fb44581b2b307321e6b79c084fcf
JIHYEON KWON <rnjswlgus98@gmail.com>
2023-08-23 11:43:24 +0900

    지역화폐 매출액 streamlit page

---
+++ page/sales_proc_page.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 16 15:36:28 2023
+
+@author: rnjsd
+"""
+
+
+import streamlit as st
+from utils import sales_processing as sp
+
+def app():
+    sp.proc()
